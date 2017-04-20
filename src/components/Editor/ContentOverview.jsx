42c96dea47f9753d9bd275e39c13fef22e1e57f7
Karolina Behalova <kaja.behalova@strv.com>
2017-04-20 16:50:26 +0200

    uvodni stranka po vytvoreni vede na editaci testu

--- src/components/Editor/ContentOverview.jsx
+++ src/components/Editor/ContentOverview.jsx
@@ -1,19 +1,32 @@
 import React, { PropTypes } from 'react'
+import { Button } from 'react-bootstrap'
 
-const ContentOverview = ({ testModel }) =>
+const ContentOverview = ({ testModel, isNew, displayGeneral }) =>
   <div>
-    <h1>{testModel.name}</h1>
-    <p>{testModel.description}</p>
-    <h3>Základní nastavení</h3>
-    <ul>
-      <li><strong>Počet otázek ve vygenerovaném testu:</strong>{testModel.questionsPerTestInstance}</li>
-      <li><strong>Časový limit testu:</strong>{testModel.timeLimit !== null ? testModel.timeLimit : 'Bez limitu'}</li>
-    </ul>
+    {isNew &&
+      <div>
+        <p>
+          Vítejte v editoru testů. Nejprve prosím vyplňte obecné údaje o testu.
+        </p>
+        <Button onClick={displayGeneral} bsSize="large" bsStyle="success">Zadat údaje o testu</Button>
+      </div>}
+    {!isNew && <div>
+        <h1>{testModel.name}</h1>
+        <p>{testModel.description}</p>
+        <h3>Základní nastavení</h3>
+        <ul>
+          <li><strong>Počet otázek ve vygenerovaném testu:</strong>{testModel.questionsPerTestInstance}</li>
+          <li><strong>Časový limit testu:</strong>{testModel.timeLimit !== null ? testModel.timeLimit : 'Bez limitu'}</li>
+        </ul>
+      </div>}
   </div>
 ContentOverview.propTypes = {
+  displayGeneral: PropTypes.func.isRequired,
+  isNew: PropTypes.bool,
   testModel: PropTypes.object,
 }
 ContentOverview.defaultProps = {
+  isNew: false,
   testModel: {},
 }
 export default ContentOverview
