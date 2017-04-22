f1a445aa17391c63413e208f943fc1a539c276c0
Karolina Behalova <kaja.behalova@strv.com>
2017-04-22 20:55:59 +0200

    tabulka - editor

--- src/components/Editor/Overview.jsx
+++ src/components/Editor/Overview.jsx
@@ -1,27 +1,50 @@
 import React, { PropTypes } from 'react'
-import { Button, Row, Col } from 'react-bootstrap'
+import { Button, Row, Col, Table } from 'react-bootstrap'
 
 const EditorOverview = props => {
   const { testModels, initModal, selectedModal, createModal, editTestHandler } = props
   return (
     <div>
-      <ul className="test-models">
+      <table className="test-models">
+        <thead>
+          <tr className="test-models__header">
+            <td>Jméno testu</td>
+            <td>Počet otázek</td>
+            <td>Možnosti</td>
+          </tr>
+        </thead>
+        <tbody>
         {testModels.map(test =>
-          <li key={test.id}>
-            {createModal(test.id, test.name, selectedModal)}
-            {test.name}
-            <Button bsStyle="success" onClick={() => editTestHandler(test.id)}>
-              Editovat
-            </Button>
-            <Button bsStyle="danger" onClick={() => initModal(test.id)}>
-              Smazat
-            </Button>
-          </li>
-          )}
-      </ul>
+          <tr key={test.id} className="test-models__item">
+            <td>
+              {test.name}
+            </td>
+            <td>
+              {test.questionModels.length}
+            </td>
+            <td>
+              <Button bsStyle="success" className="test-models__btn" onClick={() => editTestHandler(test.id)}>
+                Editovat
+              </Button>
+              <Button bsStyle="danger" className="test-models__btn" onClick={() => initModal(test.id)}>
+                Smazat
+              </Button>
+            </td>
+          </tr>)
+        }
+        </tbody>
+      </table>
     </div>
   )
 }
+      // <ul className="test-models">
+      //   {testModels.map(test =>
+      //     <li key={test.id}>
+      //       {createModal(test.id, test.name, selectedModal)}
+      //       {test.name}
+      //     </li>
+      //     )}
+      // </ul>
 EditorOverview.propTypes = {
   createModal: PropTypes.func.isRequired,
   editTestHandler: PropTypes.func.isRequired,
