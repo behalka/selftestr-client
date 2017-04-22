966a8f70b307dc038ff42b5f5bb05c05ad526972
Karolina Behalova <kaja.behalova@strv.com>
2017-04-22 20:01:42 +0200

    strankovani v otazkach

--- src/containers/Editor/QuestionsBar.jsx
+++ src/containers/Editor/QuestionsBar.jsx
@@ -5,6 +5,8 @@ import { getQuestionsByTest } from '../../redux/questionModels/questionModels.se
 import { selectQuestion } from '../../redux/editor/editor.actions'
 import { Button } from 'react-bootstrap'
 
+const QUESTIONS_PER_PAGE = 10
+
 class QuestionsBar extends Component {
   static propTypes = {
     canLeaveContent: PropTypes.func.isRequired,
@@ -21,36 +23,75 @@ class QuestionsBar extends Component {
   constructor(props) {
     super(props)
     this.questionSelectedHandler = this.questionSelectedHandler.bind(this)
+    this.moveToPage = this.moveToPage.bind(this)
+    this.buildPagination = this.buildPagination.bind(this)
+    this.state = {
+      page: 0,
+    }
+  }
+  moveToPage(page) {
+    this.setState({ page })
   }
   questionSelectedHandler(question) {
     this.props.canLeaveContent(this.props.selectQuestion.bind(this, question.id))
   }
+  buildPagination(questions) {
+    const pages = Math.ceil(questions.length / QUESTIONS_PER_PAGE)
+    const paginateBtns = []
+    for (let i = 0; i < pages; i++) {
+      const classes = classnames({
+        'editor__question': true,
+        'editor__question--selected': (i === this.state.page) && this.props.selectedQuestion,
+        'disabled': (i === this.state.page) && this.props.selectedQuestion,
+      })
+      const lower = (i * QUESTIONS_PER_PAGE) + 1
+      const upper = (i + 1) * QUESTIONS_PER_PAGE
+      paginateBtns
+        .push(
+          <Button
+            className={classes}
+            onClick={() => this.moveToPage(i)}
+            bsSize="large">{`${lower} - ${upper}`}
+          </Button>)
+    }
+    return paginateBtns
+  }
   render() {
-    /*
-     * todo: vyresit disabled zvolene otazky vic elegantne
-     *  -> idealne odstranit odkaz a upravit vzhled
-     */
-    const { isFormChanged, selectedQuestion } = this.props
+    const { isFormChanged, selectedQuestion, questions } = this.props
+    const pages = this.buildPagination(questions)
+    const lower = this.state.page * QUESTIONS_PER_PAGE
+    const upper = (this.state.page + 1) * QUESTIONS_PER_PAGE
+    const paginatedQuestions = questions
+      .slice(lower, upper)
     return (
       <nav className="editor__question-bar">
-        {this.props.questions.length === 0
+        {questions.length === 0
           && <div className="editor__question-bar--empty">
           Test zatím neobsahuje žádné otázky.
           </div>}
-        {this.props.questions.map((question, index) =>
-          <Button bsSize="large"
-            key={question.id}
-            className={
-              classnames({
-                'editor__question': true,
-                'disabled': isFormChanged && selectedQuestion !== question.id,
-                'editor__question--selected disabled': selectedQuestion === question.id,
-              })
-            }
-            onClick={() => this.questionSelectedHandler(question)}>
-            {index + 1}
-          </Button>
-          )}
+        {questions.length !== 0
+          && <div>
+            <div className="editor__questions">
+              {paginatedQuestions.map((question, index) =>
+                <Button bsSize="large"
+                  key={question.id}
+                  className={
+                    classnames({
+                      'editor__question': true,
+                      'disabled': isFormChanged && selectedQuestion !== question.id,
+                      'editor__question--selected disabled': selectedQuestion === question.id,
+                    })
+                  }
+                  onClick={() => this.questionSelectedHandler(question)}>
+                  {(this.state.page * QUESTIONS_PER_PAGE) + index + 1}
+                </Button>
+                )}
+            </div>
+            <div className="editor__pagination">
+              {pages}
+            </div>
+          </div>
+        }
       </nav>
     )
   }
