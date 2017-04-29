0960d23dc3bca488bf5f6c1c4d6e5711c4a0dcf6
Karolina Behalova <kaja.behalova@strv.com>
2017-04-29 11:37:57 +0200

    skryvaci polozky s pokrocilym nastavenim

--- src/components/Editor/General.jsx
+++ src/components/Editor/General.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { reduxForm, Field } from 'redux-form'
-import { Form, Button, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { Form, Button, OverlayTrigger, Tooltip, Collapse } from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
 
 import ContentForm from './ContentForm'
 import Input from '../forms/Input'
@@ -8,6 +9,13 @@ import validate from './GeneralValidation'
 import text from '../../constants/formHelpers'
 
 class GeneralForm extends ContentForm {
+  constructor(props) {
+    super(props)
+    this.state = {
+      advancedPanel: false,
+    }
+  }
+
   render() {
     const { handleSubmit } = this.props
     return (
@@ -15,8 +23,30 @@ class GeneralForm extends ContentForm {
         <h3 className="editor__header">Obecné nastavení</h3>
         <Field name="name" required label="Jméno testu" component={Input} type="text"/>
         <Field name="description" required label="Popis testu" component={Input} componentClass="textarea"/>
-        <Field name="timeLimit" helper={text.helpers.timeLimit} label="Časový limit (ve vteřinách)" component={Input} type="number"/>
-        <Field name="questionsPerTestInstance" helper={text.helpers.questionsPerInstance} required label="Počet otázek ve vygenerovaném testu" component={Input} type="number"/>
+        <div onClick={() => this.setState({ advancedPanel: !this.state.advancedPanel })} className="collapsible__handler">
+          <h3>
+            <FontAwesome
+              className="collapsible__icon"
+              name={this.state.advancedPanel ? 'caret-down' : 'caret-right'} />
+            Pokročilé nastavení
+          </h3>
+        </div>
+        <Collapse in={this.state.advancedPanel}>
+          <div>
+            <Field
+              name="timeLimit"
+              helper={text.helpers.timeLimit}
+              label="Časový limit (ve vteřinách)"
+              component={Input}
+              type="number"/>
+            <Field
+              name="questionsPerTestInstance"
+              helper={text.helpers.questionsPerInstance}
+              label="Počet otázek ve vygenerovaném testu"
+              component={Input}
+              type="number"/>
+          </div>
+        </Collapse>
         <Button type="submit" bsStyle="success">Uložit nastavení</Button>
         <OverlayTrigger
           placement="top"
